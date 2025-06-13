e9b2836969cc87156a3755b3118c9667a2b93b45
Rodrigo Leme <rodrigo.leme@iatec.com>
2025-06-13 10:54:39 -0300

    fix: update ScanConstants

--- android/src/main/java/com/scanlibrary/ScanConstants.java
+++ android/src/main/java/com/scanlibrary/ScanConstants.java
@@ -4,6 +4,7 @@ import android.os.Environment;
 
 /**
  * Created by jhansi on 15/03/15.
+ * Updated by Rods on 25/06/13
  */
 public class ScanConstants {
 
@@ -18,4 +19,18 @@ public class ScanConstants {
             .getExternalStorageDirectory().getPath() + "/scanSample";
 
     public final static String SELECTED_BITMAP = "selectedBitmap";
+
+    // Novas constantes para customização de texto
+    public static final String SCAN_NEXT_TEXT = "SCAN_NEXT_TEXT";
+    public static final String SCAN_SAVE_TEXT = "SCAN_SAVE_TEXT";
+    public static final String SCAN_ROTATE_LEFT_TEXT = "SCAN_ROTATE_LEFT_TEXT";
+    public static final String SCAN_ROTATE_RIGHT_TEXT = "SCAN_ROTATE_RIGHT_TEXT";
+    public static final String SCAN_ORG_TEXT = "SCAN_ORG_TEXT";
+    public static final String SCAN_BNW_TEXT = "SCAN_BNW_TEXT";
+    public static final String SCAN_SCANNING_MESSAGE = "SCAN_SCANNING_MESSAGE";
+    public static final String SCAN_LOADING_MESSAGE = "SCAN_LOADING_MESSAGE";
+    public static final String SCAN_APPLYING_FILTER_MESSAGE = "SCAN_APPLYING_FILTER_MESSAGE";
+    public static final String SCAN_CANT_CROP_ERROR_TITLE = "SCAN_CANT_CROP_ERROR_TITLE";
+    public static final String SCAN_CANT_CROP_ERROR_MESSAGE = "SCAN_CANT_CROP_ERROR_MESSAGE";
+    public static final String SCAN_OK_LABEL = "SCAN_OK_LABEL";
 }
